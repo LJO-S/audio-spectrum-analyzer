1467aa90939da65c097ffa1b1d9a8946531c6ae5
Ludvig <ludvig.snihs@outlook.com>
2025-06-01 21:33:50 +0200

    add chirp, pink and multi signals

--- fft-sim/scripts/data_gen.py
+++ fft-sim/scripts/data_gen.py
@@ -32,6 +32,18 @@ class generateData:
 
         self._generateData(a_type)
 
+        if args.type not in (
+            "sin",
+            "square",
+            "sinc",
+            "multi",
+            "chirp",
+            "am",
+            "fm",
+            "pink",
+        ):
+            raise ValueError(f"Type {args.type} not in allowed types!")
+
     def _generateData(self, a_type: str):
         tEnd = self.length * (1 / self.fs)
         time = np.arange(0, tEnd, 1 / self.fs)
@@ -43,21 +55,44 @@ class generateData:
         elif a_type == "sinc":
             self.data = np.sinc(self.freq * (time - tEnd / 2))
         elif a_type == "multi":
-            # n = 25
-            # freqs = int(self.freq) * [(0.5 + np.random.random()) for _ in range(n)]
-            # ampls = [(0.5 + np.random.random()) for _ in range(n)]
-            # phases = np.random.rand(n) * 2 * np.pi
-            # for f, A, phi in zip(freqs, ampls, phases):
-            # self.data += A * np.sin(2 * np.pi * f * time + phi)
-
-            freqs = [50e3, 52e3, 55e3, 18e3, 20e3]
-            ampls = [1.0, 0.8, 0.6, 0.4, 0.2]
+            freqs = [
+                np.random.randint(low=1e3, high=45e3)
+                for _ in range(np.random.randint(25))
+            ]
+            ampls = [np.random.uniform(low=0, high=1) for _ in range(len(freqs))]
             phases = np.random.rand(len(freqs)) * 2 * np.pi
             composite = np.zeros_like(time)
             for f, A, phi in zip(freqs, ampls, phases):
                 composite += A * np.sin(2 * np.pi * f * time + phi)
+            self.data = composite / max(abs(composite))
+        elif a_type == "chirp":
+            # Bandwidth is 50% of carrier frequency
+            B = 0.5 * self.freq
+            # Linear chirp formula: f(t) = f0 + (f1 - f0)*(t/T)
+            freqChirp = self.freq + (B / 2) * time / time[-1]
+            self.data = np.sin(2 * np.pi * freqChirp * time)
+        elif a_type == "am":
+            carrier = np.sin(2 * np.pi * self.freq * time)
+            modulator = 0.5 * np.sin(2 * np.pi * 0.2 * self.freq * time)
+            amSignal = carrier * (1 + modulator)
+            self.data = amSignal / max(abs(amSignal))
+        elif a_type == "fm":
+            beta = 10
+            phase = 2 * np.pi * self.freq * time + beta * np.sin(
+                2 * np.pi * (0.1 * self.freq) * time
+            )
+            self.data = np.cos(phase)
+        elif a_type == "pink":
+            whiteNoise = np.random.randn(self.length)
 
-            self.data = composite / 5
+            freqBins = np.fft.rfftfreq(n=self.length, d=1 / self.fs)
+            freqData = np.fft.rfft(whiteNoise)
+            # acquire pink noise by scaling with 1/sqrt(freq)
+            freqData[0] = 0
+            freqData[1:] = freqData[1:] / np.sqrt(freqBins[1:])
+            # ifft
+            pinkNoise = np.fft.irfft(freqData, self.length)
+            self.data = pinkNoise / max(abs(pinkNoise))
 
         # Acquire FFT data with real FFT
         self.fftData = np.fft.rfft(self.data, n=self.length)
@@ -152,9 +187,6 @@ if __name__ == "__main__":
     parser.add_argument("--bits", type=int, required=False, default=16)
     parser.add_argument("--store", action="store_true")
     args = parser.parse_args()
-
-    if args.type not in ("sin", "square", "sinc", "multi"):
-        raise ValueError(f"Type {args.type} not in allowed types!")
 
     obj = generateData(
         a_freq=15e3, a_length=1024, a_numBits=args.bits, a_type=args.type
