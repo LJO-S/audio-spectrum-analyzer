- Update .gitignore - Modify Python DDS tuning word calculation to match VHDL implementation -  Modify Python DDS to handle negative frequencies - Add utils plot function - Fix DDS LUT to be dual port - Update testbench write file behaviour

scripts/synth_and_test/utils.py
@@ -47,68 +47,34 @@ def compare_value(a_actual, a_reference):
             )
     return True
 
-def save_postcheck_plot(a_input_data: list, 
-                        a_output_data: list, 
-                        a_reference_data: list, 
-                        a_sampling_freq: float,
-                        a_multirate_factor: int,
-                        a_atten_db: float,
-                        a_taps_polyphase: list,
+def save_postcheck_plot_dds(
+                        a_output_data_i: list, 
+                        a_output_data_q: list, 
+                        a_ref_data_i: list, 
+                        a_ref_data_q: list, 
                         a_save_plot: bool,
                         a_output_path: str
                         ):
     # Input data
     fig = plt.figure()
     ax1 = fig.add_subplot(231)
-    ax1.plot(a_input_data)
+    ax1.plot(a_output_data_i, label="output")
+    ax1.plot(a_ref_data_i, label="ref")
+    ax1.legend(loc="lower right")
     ax1.grid(True)
-    ax1.set_title(f"Input & Output data")
+    ax1.set_title(f"In Phase")
     # Output & reference data
     ax2 = fig.add_subplot(234)
-    ax2.plot(a_output_data, label="out")
-    ax2.plot(a_reference_data, label="ref")
+    ax2.plot(a_output_data_q, label="output")
+    ax2.plot(a_ref_data_q, label="ref")
     ax2.legend(loc="lower right")
     ax2.grid(True)
-    # Input spectrum
-    ax3 = fig.add_subplot(232)
-    x_axis_freq = np.fft.rfftfreq(len(a_input_data), 1 / a_sampling_freq)
-    fft_input = np.fft.rfft(a_input_data)
-    magnitude = np.maximum(np.abs(fft_input), 1e-12)
-    magnitudeDB = 20 * np.log10(magnitude)
-    ax3.plot(x_axis_freq, magnitudeDB)
-    ax3.set_ylim(-10, 100)
-    ax3.grid(True)
-    ax3.set_title(f"Input & Output spectrum")
-    # Output spectrum
-    ax4 = fig.add_subplot(235)
-    x_axis_freq = np.fft.rfftfreq(len(a_output_data), 1 / (a_sampling_freq * a_multirate_factor))
-    fft_output = np.fft.rfft(a_output_data)
-    magnitude = np.maximum(np.abs(fft_output), 1e-12)
-    magnitudeDB = 20 * np.log10(magnitude)
-    ax4.plot(x_axis_freq, magnitudeDB)
-    ax4.set_ylim(-10, 100)
-    ax4.grid(True)
-    # Coefficients
-    ax5 = fig.add_subplot(233)
-    w, h = freqz(a_taps_polyphase, [1], worN=2000, fs=(a_sampling_freq * a_multirate_factor))
-    ax5.plot(w, 20 * np.log10(np.abs(h)))
-    ax5.set_ylim(-a_atten_db, 5)
-    ax5.grid(True)
-    ax5.set_xlabel("Frequency (Hz)")
-    ax5.set_ylabel("Gain (dB)")
-    ax5.set_title(f"Coeff Freq Resp & Coeffs")
-    ax6 = fig.add_subplot(236)
-    ax6.plot(a_taps_polyphase)
-    ax6.grid(True)
-    ax6.set_xlabel("Coefficient Index")
-    ax6.set_ylabel("N/A")
     if a_save_plot:
         fig.set_size_inches(32, 18)
         plt.savefig(
             str(Path(a_output_path))
             + "/"
             + f"results.svg",
-            # bbox_inches="tight",
             dpi=1000
         )
     else: